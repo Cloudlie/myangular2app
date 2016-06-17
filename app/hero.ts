d3f068e98c53b2776bcfd8b89c346047cc91174b
unknown <luomingwei@luomingwei-wms.yt.corp>
2016-06-17 19:17:47 +0800

    commit

---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by luomingwei on 2016/6/17.
+ */
+export class Hero {
+    id:number;
+    name:string;
+}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by luomingwei on 2016/6/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
+
